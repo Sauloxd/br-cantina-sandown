38ec58cfd933a42c5cc2b02fa457e3b2f1d97f55
Saulo Furuta <saulotoshi@gmail.com>
2015-09-08 20:03:55 -0300

    added few node modules, plus a directory tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-gulpfile.js
+var gulp 	  = require('gulp');
+var uglify 	= require('gulp-uglify');
+var concat  = require('gulp-concat');
+var jade    = require('gulp-jade');
+
+gulp.task('scripts', function(){
+	gulp.src('src/assets/js/*.js')
+	 .pipe(concat('all.min.js'))
+	 .pipe(uglify())
+	 .pipe(gulp.dest('dist/js'));
+  gulp.src('src/app/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('dist/html'));
+});
